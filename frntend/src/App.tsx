0bf61625ae461fad00417d36c3a9548e4b7a10dc
kh-hamz <hamzasidd159@gmail.com>
2025-03-08 09:44:08 -0800

    Added navbar elements

--- frntend/src/App.tsx
+++ frntend/src/App.tsx
@@ -1,10 +1,24 @@
 import './App.css'
+import logo from './assets/react.svg' 
 
 function App() {
   return (
     <>
         <div className='navbar'>
-            <h5>GeMorph</h5>  
+          <div className='nav-inner'>
+            <div className='logo-bar'>
+              <img src={logo} alt="Logo" />
+              <h5>GeMorph</h5>              
+            </div>
+            <div className='nav-links'>
+              <a href="#">About</a>
+              <a href="#">Services</a>
+              <a href="#">Contact</a>
+            </div>
+            <button className='login-button'>
+              Login
+            </button>
+          </div>
         </div>
     </>
   )
